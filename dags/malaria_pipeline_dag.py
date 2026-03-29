afd2370423ec6cdd0cf7ad4f1bcbd8529e875d6f
onchonga-programmer <bochaberibrenda03@gmail.com>
2026-03-29 21:38:58 +0300

    correct airflow error

--- dags/malaria_pipeline_dag.py
+++ dags/malaria_pipeline_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
 from docker.types import Mount
+import os
 
 default_args = {
     "owner": "airflow",
@@ -20,22 +21,22 @@ with DAG(
 ) as dag:
 
     ingest = DockerOperator(
-        task_id="bronze_ingestion",
-        image="malaria-ingestion",
-        command="python run_ingestion.py",
-        docker_url="unix:///var/run/docker.sock",
-        network_mode="bridge",
-        auto_remove=True,
-        environment={
-            "AWS_ACCESS_KEY_ID": "{{ var.value.AWS_ACCESS_KEY_ID }}",
-            "AWS_SECRET_ACCESS_KEY": "{{ var.value.AWS_SECRET_ACCESS_KEY }}",
-            "S3_BUCKET": "{{ var.value.S3_BUCKET }}",
-        },
-        mounts=[
-            Mount(source="/home/brenda/Global_Malaria_burden_predicting_system/logs",
-                  target="/app/logs", type="bind")
-        ],
-    )
+    task_id="bronze_ingestion",
+    image="malaria-ingestion",
+    command="python run_ingestion.py",
+    docker_url="unix:///var/run/docker.sock",
+    network_mode="bridge",
+    auto_remove=True,
+    environment={
+        "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
+        "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
+        "S3_BUCKET": os.environ.get("S3_BUCKET", "malaria-forecast-bree"),
+    },
+    mounts=[
+        Mount(source="/home/brenda/Global_Malaria_burden_predicting_system/logs",
+              target="/app/logs", type="bind")
+    ],
+)
 
     transform = DockerOperator(
         task_id="silver_gold_transformation",
